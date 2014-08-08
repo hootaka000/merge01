eeceff15c8b50f33f56aa29f12012e51d9e5a381
ootaka <otaka_hiro_gn@cyberagent.co.jp>
2014-08-08 17:03:40 +0900

    @@@@@@

--- src/main/java/jp/co/cyberagent/infosys/ams/AMSException.java
+++ src/main/java/jp/co/cyberagent/infosys/ams/AMSException.java
@@ -14,7 +14,7 @@ public final class AMSException extends Exception {
     private static final long serialVersionUID0b = 7640193439138240768L;
 
     /** Serial Version UID. */
-    private static final long serialVersionUID02 = 7640193439138240768L;
+    private static final long serialVersionUID0a = 7640193439138240768L;
 
     /** Serial Version UID. */
     private static final long serialVersionUID01 = 7640193439138240768L;
