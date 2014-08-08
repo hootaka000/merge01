b7582b67ea52eb5a1c3e2391c85ed794cc7c298d
ootaka <otaka_hiro_gn@cyberagent.co.jp>
2014-08-08 17:16:30 +0900

    @@@@

---
+++ src/main/java/jp/co/cyberagent/infosys/ams/AMSException.java
@@ -0,0 +1,40 @@
+package jp.co.cyberagent.infosys.ams;
+
+/**
+ * 汎用例外クラス.
+ * @author shimizu.kenji
+ *
+ */
+public final class AMSException extends Exception {
+
+    /** Serial Version UID. */
+    private static final long serialVersionUID = 7640193439138240768L;
+
+    /** Serial Version UID. */
+    private static final long serialVersionUID0b = 7640193439138240768L;
+
+    /** Serial Version UID. */
+    private static final long serialVersionUIDaa = 7640193439138240768L;
+
+    /** Serial Version UID. */
+    private static final long serialVersionUID01 = 7640193439138240768L;
+
+    /** Serial Version UID. */
+    private static final long serialVersionUID03 = 7640193439138240768L;
+
+    /**
+     * メッセージを指定して例外オブジェクトを生成する.
+     * @param message メッセージ
+     */
+    public AMSException(final String message) {
+        super(message);
+    }
+    /**
+     * メッセージと原因となった例外を指定して例外オブジェクトを生成する.
+     * @param message メッセージ
+     * @param cause 例外オブジェクト
+     */
+    public AMSException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
